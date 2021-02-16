0f66959e897c5e18e692347ceb38eebe869daba7
Laika Deunert <laikadeunert@Laikas-Laptop.attlocal.net>
2021-02-16 13:14:06 -0800

    commit tree 7bd894cbe02a0c43f36179c4ac25e78c95d57ba6

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,30 @@
+class Doctor
+  
+    attr_accessor :name, :appointments, :patient
 
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @appointments = []
+      @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+  
+    def new_appointment(date, patient)
+       appointment = Appointment.new(patient, date, self)
+       @appointments << appointment
+       appointment
+    end
+
+    def appointments 
+        Appointment.all.select{|appointment|appointment.doctor == self}
+    end
+  
+    def patients
+        Patient.all.collect{|patient|patient}
+    end
+  end
